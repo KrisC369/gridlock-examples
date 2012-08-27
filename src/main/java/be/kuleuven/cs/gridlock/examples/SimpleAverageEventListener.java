37d231ace9e0ea65465bb9e753a5535fd5c3f4fa
Kristof Coninx <kristofconinx369@gmail.com>
2012-08-28 00:03:09 +0200

    Removing anoying templete comment headers from source files.

--- src/main/java/be/kuleuven/cs/gridlock/examples/SimpleAverageEventListener.java
+++ src/main/java/be/kuleuven/cs/gridlock/examples/SimpleAverageEventListener.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package be.kuleuven.cs.gridlock.examples;
 
 import be.kuleuven.cs.gridlock.simulation.events.Event;
--- src/main/java/be/kuleuven/cs/gridlock/examples/csvgenerator/SimpleEvenlyDistributedTrafficGenerator.java
+++ src/main/java/be/kuleuven/cs/gridlock/examples/csvgenerator/SimpleEvenlyDistributedTrafficGenerator.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package be.kuleuven.cs.gridlock.examples.csvgenerator;
 
 import be.kuleuven.cs.gridlock.configuration.Configuration;
